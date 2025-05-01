46acd22715fb2575fc9fa2b423661f8a90f53b9f
David NYIRINGABO <nyiringabodavid62@gmail.com>
2025-05-01 16:29:57 +0200

    feat: change reset password logic & add code verification

--- src/prisma/prisma-client.ts
+++ src/prisma/prisma-client.ts
@@ -1,21 +1,23 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 declare global {
-    var prisma: PrismaClient | undefined;
+  var prisma: PrismaClient | undefined;
 }
 
-const prisma = global.prisma || new PrismaClient({
-    log: ['query', 'error', 'warn'],
-    errorFormat: 'minimal',
+const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ["query", "error", "warn"],
+    errorFormat: "minimal",
     datasources: {
-        db: {
-            url: process.env.DATABASE_URL
-        }
-    }
-});
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
 
-if (process.env.NODE_ENV !== 'production') {
-    global.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
 }
 
 export default prisma;
